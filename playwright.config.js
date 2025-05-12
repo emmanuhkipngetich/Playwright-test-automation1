e6762e92112e88ce2489120fa3ed0d8347af79bb
emmanuhkipngetich <emmmanuhkipngetich@gmail.com>
2025-05-12 04:19:33 +0300

    Latest

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './Test',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -39,15 +39,15 @@ module.exports = defineConfig({
     },
 
    
-   {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+   //{
+     // name: 'firefox',
+     // use: { ...devices['Desktop Firefox'] },
+    //},
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    //{
+      //name: 'webkit',
+      //use: { ...devices['Desktop Safari'] },
+    //},
     
   
     /* Test against mobile viewports. */
